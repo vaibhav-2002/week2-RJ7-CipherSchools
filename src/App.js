efc513a19ff2832677599a17623d3893b44960b6
vaibhav-2002 <vaibhavkhandelwal2002@gmail.com>
2023-02-03 17:48:01 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import "./App.css";
-import AllUserList from "./component/AllUserList";
-import Test from "./Test";
-import Name from "./component/Name";
-import SignUp from "./SignUpForm";
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import SignUpForm from "./SignUpForm";
+
+const App = () => {
   return (
-    <div className="App">
-      <Test></Test>
-      <AllUserList></AllUserList>
-      {/* <Name></Name> */}
-      <SignUp></SignUp>
+    <div>
+      <BrowserRouter>
+        <Header></Header>
+
+        <Routes>
+          <Route path={"/"} element={<h1>This is Home Page</h1>} />
+          <Route
+            path={"/contact"}
+            element={<h1>This is Contact page</h1>}
+          ></Route>
+          <Route
+            path={"/about"}
+            element={<h1>This is an About Page</h1>}
+          ></Route>
+          <Route path={"/signup"} element={<SignUpForm></SignUpForm>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
-
+};
 export default App;
